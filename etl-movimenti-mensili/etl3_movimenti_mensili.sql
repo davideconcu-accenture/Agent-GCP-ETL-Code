70b96a6fa53b68d6a3ed832a4021a0d5d940a539
davideconcu-accenture <davide.concu@accenture.com>
2026-04-27 11:47:09 +0200

    fix(etl-movimenti): Correct channel name for aggregation

--- etl-movimenti-mensili/etl3_movimenti_mensili.sql
+++ etl-movimenti-mensili/etl3_movimenti_mensili.sql
@@ -67,7 +67,7 @@ SELECT
   COUNTIF(canale = 'WEB')                           AS num_ops_web,
   COUNTIF(canale = 'POS')                           AS num_ops_pos,
   COUNTIF(canale = 'ATM')                           AS num_ops_atm,
-  COUNTIF(canale = 'FILIALE')                       AS num_ops_filiale,
+  COUNTIF(canale = 'BANCA')                         AS num_ops_filiale, -- FIX: 'FILIALE' era sbagliato, il valore corretto è 'BANCA'
 
   -- Mix canale digitale
   SAFE_DIVIDE(COUNTIF(canale IN ('APP','WEB')),
