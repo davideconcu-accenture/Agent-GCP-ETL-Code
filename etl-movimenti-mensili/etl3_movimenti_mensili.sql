9efdcb083ea7b52c23091e6549989f51e337e7ef
davideconcu-accenture <davide.concu@accenture.com>
2026-04-27 11:02:59 +0200

    fix(etl-movimenti): Corretto il conteggio del canale 'BANCA' al posto di 'FILIALE'

--- etl-movimenti-mensili/etl3_movimenti_mensili.sql
+++ etl-movimenti-mensili/etl3_movimenti_mensili.sql
@@ -67,7 +67,7 @@ SELECT
   COUNTIF(canale = 'WEB')                           AS num_ops_web,
   COUNTIF(canale = 'POS')                           AS num_ops_pos,
   COUNTIF(canale = 'ATM')                           AS num_ops_atm,
-  COUNTIF(canale = 'FILIALE')                       AS num_ops_filiale,
+  COUNTIF(canale = 'BANCA')                         AS num_ops_filiale, -- BUG FIX: Corretto 'FILIALE' in 'BANCA' per rispecchiare i dati sorgente
 
   -- Mix canale digitale
   SAFE_DIVIDE(COUNTIF(canale IN ('APP','WEB')),
