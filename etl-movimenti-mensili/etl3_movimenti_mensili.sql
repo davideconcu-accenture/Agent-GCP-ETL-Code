219ee5b40aad132c92d0c8e6147afa054bc3c624
davideconcu-accenture <davide.concu@accenture.com>
2026-04-29 17:12:47 +0200

    fix(etl-movimenti): corretto il conteggio dei canali

--- etl-movimenti-mensili/etl3_movimenti_mensili.sql
+++ etl-movimenti-mensili/etl3_movimenti_mensili.sql
@@ -67,7 +67,7 @@ SELECT
   COUNTIF(canale = 'WEB')                           AS num_ops_web,
   COUNTIF(canale = 'POS')                           AS num_ops_pos,
   COUNTIF(canale = 'ATM')                           AS num_ops_atm,
-  COUNTIF(canale = 'FILIALE')                       AS num_ops_filiale,
+  COUNTIF(canale = 'BANCA')                       AS num_ops_banca,
 
   -- Mix canale digitale
   SAFE_DIVIDE(COUNTIF(canale IN ('APP','WEB')),
